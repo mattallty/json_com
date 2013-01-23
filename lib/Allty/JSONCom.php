151f4ccb49c8999c0ed5e2ccfc9dd9028f8686de
Matt <matt@allty.com>
2013-01-23 16:33:34 +0100

    Fix NS calls

--- lib/Allty/JSONCom.php
+++ lib/Allty/JSONCom.php
@@ -33,19 +33,19 @@ class JSONCom {
      */
     public static function decode($string, $assoc = false, $depth = 512, $options = 0) {
         
-        if(!is_string($string)) {
-            throw new JSONComInvalidArgumentException('Parameter $str must be a string. "'.  gettype($string).'" given.');
+        if(!\is_string($string)) {
+            throw new JSONComInvalidArgumentException('Parameter $str must be a string. "'.  \gettype($string).'" given.');
         }
         
         $json = self::removeComments($string);
         
         // PHP > 5.4.0 supports the 4th parameter ($options)
-        if(version_compare(PHP_VERSION, '5.4.0', '>=')) {
-            return json_decode($json, $assoc, $depth, $options);
+        if(\version_compare(\PHP_VERSION, '5.4.0', '>=')) {
+            return \json_decode($json, $assoc, $depth, $options);
         }
         
         // PHP < 5.4.0 does not support the 4th parameter
-        return json_decode($json, $assoc, $depth);
+        return \json_decode($json, $assoc, $depth);
     }
     
     /**
@@ -57,7 +57,7 @@ class JSONCom {
      */
     protected static function removeComments($string)
     {
-        if(!is_string($string) || empty($string)) {
+        if(!\is_string($string) || empty($string)) {
             return '';
         }
         return preg_replace("%/\*(?:(?!\*/).)*\*/%s", '', 
